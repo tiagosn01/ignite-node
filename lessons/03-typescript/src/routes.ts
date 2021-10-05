5bcb6fff1c713ea2d09cda55c8713112a29c5304
Tiago Silva <tiagosilva0922@gmail.com>
2021-10-05 01:03:22 -0300

    feat: lessons 03 typescript project configuration

---
+++ lessons/03-typescript/src/routes.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from 'express';
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(request: Request, response: Response) {
+  CreateCourseService.execute({name: "NodeJS", duration: 10, educator: "Dani"});
+  CreateCourseService.execute({name: "NodeJS", educator: "Tiago"});
+
+  return response.send();
+}
